4d9951bc994612d40eef3d66b2106afe66b94604
Swapnil Sable <swapnil.sable@outlook.com>
2019-06-30 19:07:22 +0530

    add password digest migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_095553) do
+ActiveRecord::Schema.define(version: 2019_06_30_132756) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_30_095553) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
